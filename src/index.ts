6c7c6751e7c8cb0c8b4a2c1b4f216219fadc5bfa
昌维 <867597730@qq.com>
2020-11-08 15:37:49 +0800

    add type: ReactElement, VirtualNode, VirtualTextNode, export unmount

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import createElement from "./createElement";
-import Component from "./Component";
+import Component, {create, setProps, unmount} from "./Component";
 import {FC} from "./FC";
 
 export type ReactComponent<P, S> = FC<P> | Component<P, S>;
 
-export type VirtualDOM = {
-  tagName: string | ReactComponent<any, any>
-  attributes?: Partial<HTMLElement>
-  children?: Array<VirtualDOM | number | string | boolean>
+export interface ReactElement extends Partial<Node> {
+  key?: string | number
+  _node?: Node
 }
 
+export interface ComponentElement<P, S> extends ReactElement {
+  _instance?: Component<P, S>
+}
+
+export type VirtualTextNode = number | string | boolean
+export type VirtualNode = VirtualDOM | VirtualTextNode
+
+export interface VirtualDOM {
+  tagName: string | ReactComponent<any, any>
+  attributes?: ComponentElement<any, any>
+  children?: VirtualNode[]
+}
+
+
 export default {createElement, Component};
-export {Component, FC};
+export {Component, FC, create, setProps, unmount};
