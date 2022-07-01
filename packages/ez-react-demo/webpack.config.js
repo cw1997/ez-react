131a3b81bf31f1e2c06b7fabe335a2b2005b6fd5
Chang Wei <changwei1006@gmail.com>
2022-07-01 21:21:40 +0800

    Revert "chore: remove unused webpack plugin 'mini-css-extract-plugin'"
    This reverts commit f72ced1132ed8cddb80819289304bf96a73d7f42.

--- packages/ez-react-demo/webpack.config.js
+++ packages/ez-react-demo/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 module.exports = {
